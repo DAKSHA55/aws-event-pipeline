4ca2b4f6f0344a8e914fdd666e077da477ca59dc
DAKSHA55 <kshitijpatil63@gmail.com>
2025-07-26 14:25:10 +0530

    Cleaned up .terraform and added .gitignore

--- main.tf
+++ main.tf
@@ -1,75 +1,51 @@
 provider "aws" {
-  region = "ap-south-1"  # Change if you're in a different region
+  region = "ap-south-1" # Mumbai
 }
 
-# Input S3 Bucket
 resource "aws_s3_bucket" "input_bucket" {
-  bucket        = "csv-input-bucket-kshitij"
-  force_destroy = true
+  bucket = "csv-input-bucket-kshitij"
 }
 
-# Output S3 Bucket
 resource "aws_s3_bucket" "output_bucket" {
-  bucket        = "csv-output-bucket-kshitij"
-  force_destroy = true
+  bucket = "csv-output-bucket-kshitij"
 }
 
-# Lambda IAM Role
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda_execution_role"
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda"
+  output_path = "${path.module}/function.zip"
+}
 
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
       Action = "sts:AssumeRole",
+      Effect = "Allow",
       Principal = {
         Service = "lambda.amazonaws.com"
-      },
-      Effect = "Allow",
-      Sid    = ""
+      }
     }]
   })
 }
 
-# Attach basic Lambda execution policy
-resource "aws_iam_policy_attachment" "lambda_basic_execution" {
-  name       = "attach-lambda-basic"
-  roles      = [aws_iam_role.lambda_exec_role.name]
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# Lambda Function
 resource "aws_lambda_function" "csv_processor" {
-  filename         = "function.zip"
   function_name    = "csvProcessorFunction"
   role             = aws_iam_role.lambda_exec_role.arn
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.9"
-  source_code_hash = filebase64sha256("function.zip")
-  timeout          = 10
-
-  environment {
-    variables = {
-      OUTPUT_BUCKET = aws_s3_bucket.output_bucket.bucket
-    }
-  }
-
-  depends_on = [
-    aws_iam_policy_attachment.lambda_basic_execution
-  ]
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  timeout          = 30
 }
 
-# Allow S3 to invoke Lambda
-resource "aws_lambda_permission" "allow_s3" {
-  statement_id  = "AllowS3Invoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.csv_processor.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.input_bucket.arn
-}
-
-# S3 Notification to trigger Lambda
-resource "aws_s3_bucket_notification" "input_bucket_notification" {
+resource "aws_s3_bucket_notification" "s3_trigger" {
   bucket = aws_s3_bucket.input_bucket.id
 
   lambda_function {
@@ -79,4 +55,12 @@ resource "aws_s3_bucket_notification" "input_bucket_notification" {
   }
 
   depends_on = [aws_lambda_permission.allow_s3]
+}
+
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.csv_processor.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.input_bucket.arn
 }
